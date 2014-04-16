a913600ed84713d0dccc87292ea661f945715da2
Inu <maximin.deschamps@laposte.net>
2014-04-16 20:15:47 +0200

    Premiers elements

--- Terre Natale Calculateur/Form1.Designer.cs
+++ Terre Natale Calculateur/Form1.Designer.cs
@@ -30,39 +30,168 @@ namespace Terre_Natale_Calculateur
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.characterName = new System.Windows.Forms.Label();
-            this.talentsBox = new System.Windows.Forms.FlowLayoutPanel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.Stats = new System.Windows.Forms.TabPage();
+            this.TalentsG = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bar_discretion = new System.Windows.Forms.ProgressBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.TalentsM = new System.Windows.Forms.TabPage();
+            this.TalentsA = new System.Windows.Forms.TabPage();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1.SuspendLayout();
+            this.TalentsG.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // characterName
             // 
             this.characterName.AutoSize = true;
-            this.characterName.Location = new System.Drawing.Point(12, 9);
-            this.characterName.Dock = DockStyle.Top;
+            this.characterName.Dock = System.Windows.Forms.DockStyle.Top;
+            this.characterName.Location = new System.Drawing.Point(0, 0);
             this.characterName.Name = "characterName";
             this.characterName.Size = new System.Drawing.Size(84, 13);
             this.characterName.TabIndex = 0;
             this.characterName.Text = "Character Name";
             // 
-            // talentsBox
+            // tabControl1
             // 
-            this.talentsBox.AutoSize = true;
-            this.talentsBox.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.talentsBox.Dock = DockStyle.Fill;
-            this.talentsBox.Location = new System.Drawing.Point(15, 25);
-            this.talentsBox.Name = "talentsBox";
-            this.talentsBox.Size = new System.Drawing.Size(0, 0);
-            this.talentsBox.TabIndex = 1;
+            this.tabControl1.Controls.Add(this.Stats);
+            this.tabControl1.Controls.Add(this.TalentsG);
+            this.tabControl1.Controls.Add(this.TalentsM);
+            this.tabControl1.Controls.Add(this.TalentsA);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 13);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(522, 427);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // Stats
+            // 
+            this.Stats.Location = new System.Drawing.Point(4, 22);
+            this.Stats.Name = "Stats";
+            this.Stats.Padding = new System.Windows.Forms.Padding(3);
+            this.Stats.Size = new System.Drawing.Size(514, 401);
+            this.Stats.TabIndex = 0;
+            this.Stats.Text = "Stats";
+            this.Stats.UseVisualStyleBackColor = true;
+            // 
+            // TalentsG
+            // 
+            this.TalentsG.Controls.Add(this.groupBox1);
+            this.TalentsG.Location = new System.Drawing.Point(4, 22);
+            this.TalentsG.Name = "TalentsG";
+            this.TalentsG.Padding = new System.Windows.Forms.Padding(3);
+            this.TalentsG.Size = new System.Drawing.Size(514, 401);
+            this.TalentsG.TabIndex = 1;
+            this.TalentsG.Text = "Talents Generaux";
+            this.TalentsG.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.bar_discretion);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(8, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(247, 42);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "discretion";
+            // 
+            // bar_discretion
+            // 
+            this.bar_discretion.ContextMenuStrip = this.contextMenuStrip1;
+            this.bar_discretion.Location = new System.Drawing.Point(110, 11);
+            this.bar_discretion.MarqueeAnimationSpeed = 1;
+            this.bar_discretion.Name = "bar_discretion";
+            this.bar_discretion.Size = new System.Drawing.Size(100, 23);
+            this.bar_discretion.Step = 1;
+            this.bar_discretion.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.bar_discretion.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(216, 11);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(24, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "+";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(80, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(24, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "-";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TalentsM
+            // 
+            this.TalentsM.Location = new System.Drawing.Point(4, 22);
+            this.TalentsM.Name = "TalentsM";
+            this.TalentsM.Size = new System.Drawing.Size(514, 401);
+            this.TalentsM.TabIndex = 2;
+            this.TalentsM.Text = "Talents Martiaux";
+            this.TalentsM.UseVisualStyleBackColor = true;
+            // 
+            // TalentsA
+            // 
+            this.TalentsA.Location = new System.Drawing.Point(4, 22);
+            this.TalentsA.Name = "TalentsA";
+            this.TalentsA.Size = new System.Drawing.Size(514, 401);
+            this.TalentsA.TabIndex = 3;
+            this.TalentsA.Text = "Talents Aptitudes";
+            this.TalentsA.UseVisualStyleBackColor = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(92, 26);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(91, 22);
+            this.toolStripMenuItem2.Text = "1/5";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(351, 315);
-            this.Controls.Add(this.talentsBox);
+            this.ClientSize = new System.Drawing.Size(522, 440);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.characterName);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.TalentsG.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -71,7 +200,18 @@ namespace Terre_Natale_Calculateur
         #endregion
 
         private System.Windows.Forms.Label characterName;
-        private System.Windows.Forms.FlowLayoutPanel talentsBox;
+        private TabControl tabControl1;
+        private TabPage Stats;
+        private TabPage TalentsG;
+        private TabPage TalentsM;
+        private TabPage TalentsA;
+        private GroupBox groupBox1;
+        private Label label1;
+        private ProgressBar bar_discretion;
+        private Button button2;
+        private Button button1;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem toolStripMenuItem2;
     }
 }
 
--- Terre Natale Calculateur/Form1.cs
+++ Terre Natale Calculateur/Form1.cs
@@ -8,23 +8,7 @@ namespace Terre_Natale_Calculateur
         public Form1()
         {
             InitializeComponent();
-            var character = new Character("Cyrus", new TalentsFactory());
-            characterName.Text = character.Name;
-
-            SuspendLayout();
-
-            foreach (Talent talent in character.Talents)
-            {
-                var talentLabel = new Label();
-                talentLabel.Text = string.Format("{0}: {1} ({2})", talent.Name, talent.Level, talent.XPCost);
-                talentLabel.Anchor = AnchorStyles.Top | AnchorStyles.Left;
-                talentsBox.Controls.Add(talentLabel);
-                Console.WriteLine("Added talent: {0}", talent.Name);
-            }
-            ResumeLayout(false);
-            PerformLayout();
-
-            this.Resize += resizeHandler;
+           
         }
 
         private void resizeHandler(object sender, EventArgs eventArgs)
@@ -32,5 +16,28 @@ namespace Terre_Natale_Calculateur
             PerformAutoScale();
             PerformLayout();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (bar_discretion.Value > 0)
+            {
+                bar_discretion.Value = bar_discretion.Value - 20;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (bar_discretion.Value < bar_discretion.Maximum)
+            {
+                bar_discretion.Value = bar_discretion.Value+20;
+            }
+        }
+
+       
     }
 }
