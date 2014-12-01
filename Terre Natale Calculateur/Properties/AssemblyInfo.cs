89037e74690a274a11e041e8afb8ffe0916b1216
Julien Garagnon <jupotter.g@gmail.com>
2014-12-01 22:28:58 +0100

    Update version

--- Terre Natale Calculateur/Properties/AssemblyInfo.cs
+++ Terre Natale Calculateur/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.3.9.*")]
+[assembly: AssemblyFileVersion("1.3.9.*")]
 
 
 // Make internal classes visible to test project
