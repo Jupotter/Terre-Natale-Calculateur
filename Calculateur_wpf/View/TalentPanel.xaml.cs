ab667f8ee659a14cfef60f22f213fe512835a6d1
Julien Garagnon <jupotter.g@gmail.com>
2014-12-08 15:26:21 +0100

    Start work on talent paner

---
+++ Calculateur_wpf/View/TalentPanel.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Calculateur_wpf.View
+{
+    /// <summary>
+    /// Logique d'interaction pour TalentPanel.xaml
+    /// </summary>
+    public partial class TalentPanel : UserControl
+    {
+        public TalentPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Calculateur_wpf/ViewModel/TalentsPanel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Practices.Prism.Mvvm;
+using System.Collections.Generic;
+using Terre_Natale_Calculateur;
+
+namespace Calculateur_wpf.ViewModel
+{
+    class TalentsPanel : BindableBase
+    {
+        private Character character;
+
+        public IEnumerable<Talent> Talents
+        {
+            get { return character == null ? null : character.Talents; }
+        }
+
+        public TalentsPanel()
+        {
+            CharacterManager.CharacterChanged += CharacterManager_OnCharacterChanged;
+        }
+
+        private void CharacterManager_OnCharacterChanged(Character caller)
+        {
+            character = caller;
+            OnPropertyChanged(null);
+        }
+    }
+}
