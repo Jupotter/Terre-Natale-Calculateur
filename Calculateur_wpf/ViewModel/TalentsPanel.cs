ef75686bc3137ec69b233fecd0dec381dbcc5eaf
Julien Garagnon <jupotter.g@gmail.com>
2014-12-08 18:12:29 +0100

    REwork talentPanel

--- Calculateur_wpf/ViewModel/TalentsPanel.cs
+++ Calculateur_wpf/ViewModel/TalentsPanel.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Practices.Prism.Mvvm;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using Terre_Natale_Calculateur;
 
 namespace Calculateur_wpf.ViewModel
@@ -8,9 +10,27 @@ namespace Calculateur_wpf.ViewModel
     {
         private Character character;
 
-        public IEnumerable<Talent> Talents
+        public IEnumerable<KeyValuePair<Aspect, List<Talent>>> Talents
         {
-            get { return character == null ? null : character.Talents; }
+            get
+            {
+                if (character == null) return null;
+                var talents = character.Talents;
+
+                var ret = new Dictionary<Aspect, List<Talent>>();
+                foreach (var aspect in from aspect in ((Aspect[]) Enum.GetValues(typeof (Aspect)))
+                    where aspect != Aspect.None && aspect != Aspect.Equilibre
+                    select aspect)
+                {
+                    ret[aspect] = new List<Talent>();
+                    foreach (var talent in talents)
+                    {
+                        if (talent.PrimaryAspect == aspect && talent.SecondaryAspect == Aspect.None)
+                            ret[aspect].Add(talent);
+                    }
+                }
+                return ret;
+            }
         }
 
         public TalentsPanel()
