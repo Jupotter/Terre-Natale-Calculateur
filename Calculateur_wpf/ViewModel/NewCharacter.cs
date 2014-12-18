8a0f5702359da52308edd1362ad8673dec9a3eb7
Jupotter <jupotter.g@gmail.com>
2014-12-18 16:44:12 +0100

    Start working on new character window

---
+++ Calculateur_wpf/ViewModel/NewCharacter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Prism.Mvvm;
+using Terre_Natale_Calculateur;
+
+namespace Calculateur_wpf.ViewModel
+{
+    class NewCharacter : BindableBase
+    {
+        public string Name { get; set; }
+
+        public IEnumerable<Race> Races
+        {
+            get { return RacesManager.Instance.CreateSet().Values; }
+        }
+    }
+}
