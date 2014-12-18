6f9aff8450febdfaefaf0d132697a2e8e1ef2968
Julien Garagnon <jupotter.g@gmail.com>
2014-12-18 11:35:32 +0100

    Work on talent panel in WPF

---
+++ Calculateur_wpf/ViewModel/TalentGroupBox.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Practices.Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Terre_Natale_Calculateur;
+
+namespace Calculateur_wpf.ViewModel
+{
+    class TalentGroupBox : BindableBase
+    {
+        public delegate bool TalentTest(Talent t);
+
+        private Character character;
+        private TalentTest talentTest;
+
+        public TalentGroupBox()
+        {
+            CharacterManager.CharacterChanged += CharacterManager_OnCharacterChanged;
+        }
+
+        public TalentGroupBox(Character character)
+            : this()
+        {
+            this.character = character;
+        }
+
+        public IEnumerable<Talent> Talents
+        {
+            get
+            {
+                if (character == null)
+                    return null;
+                var ret = character.Talents.Where(talent => talentTest(talent)).ToList();
+                return ret.Count == 0 ? null : ret;
+            }
+        }
+
+        public String Name { get; set; }
+
+        public void SetTalentsOption(TalentTest test)
+        {
+            talentTest = test;
+        }
+
+        private void CharacterManager_OnCharacterChanged(Character caller)
+        {
+            character = caller;
+            OnPropertyChanged(null);
+        }
+    }
+}
--- Calculateur_wpf/ViewModel/TalentsPanel.cs
+++ Calculateur_wpf/ViewModel/TalentsPanel.cs
@@ -10,22 +10,45 @@ namespace Calculateur_wpf.ViewModel
     {
         private Character character;
 
-        public IEnumerable<KeyValuePair<Aspect, List<Talent>>> Talents
+        public IEnumerable<TalentGroupBox> TalentsGeneraux
         {
             get
             {
                 if (character == null) return null;
                 var talents = character.Talents;
 
-                var ret = new Dictionary<Aspect, List<Talent>>();
+                var ret = new List<TalentGroupBox>();
                 foreach (var aspect in from aspect in ((Aspect[]) Enum.GetValues(typeof (Aspect)))
                     where aspect != Aspect.None && aspect != Aspect.Equilibre
                     select aspect)
                 {
+                    var box = new TalentGroupBox(character);
+                    var local = aspect;
+                    box.SetTalentsOption(talent => talent.PrimaryAspect == local && talent.SecondaryAspect == Aspect.None);
+                    box.Name = String.Format("Talents de {0}", aspect);
+                    ret.Add(box);
+                }
+                return ret;
+            }
+        }
+
+        public IEnumerable<KeyValuePair<Aspect, List<Talent>>> TalentsAptitude
+        {
+            get
+            {
+                if (character == null)
+                    return null;
+                var talents = character.Talents;
+
+                var ret = new Dictionary<Aspect, List<Talent>>();
+                foreach (var aspect in from aspect in ((Aspect[])Enum.GetValues(typeof(Aspect)))
+                                       where aspect != Aspect.None && aspect != Aspect.Equilibre
+                                       select aspect)
+                {
                     ret[aspect] = new List<Talent>();
                     foreach (var talent in talents)
                     {
-                        if (talent.PrimaryAspect == aspect && talent.SecondaryAspect == Aspect.None)
+                        if (talent.PrimaryAspect == aspect && talent.Type == TalentType.Aptitude)
                             ret[aspect].Add(talent);
                     }
                 }
