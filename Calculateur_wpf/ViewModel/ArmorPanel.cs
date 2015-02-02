f5c69a61291c2facb4c5dba3854d22da217b6159
Julien Garagnon <jupotter.g@gmail.com>
2015-02-02 15:40:30 +0100

    Start Armor Panel

---
+++ Calculateur_wpf/ViewModel/ArmorPanel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Practices.Prism.Mvvm;
+using System.Collections.Generic;
+
+namespace Calculateur_wpf.ViewModel
+{
+    class ArmorPanel : BindableBase
+    {
+        public enum ArmorType
+        {
+            Tissus,
+            Cuir,
+            Maille,
+            Plaque
+        }
+
+        public static IEnumerable<ArmorType> ArmorTypes
+        {
+            get { return new[] {ArmorType.Tissus, ArmorType.Cuir, ArmorType.Maille, ArmorType.Plaque}; }
+        }
+    }
+}
