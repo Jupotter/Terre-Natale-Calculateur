0c8e6409116f19e6a7953880ee009ff9b914007d
Julien Garagnon <jupotter.g@gmail.com>
2015-02-10 14:08:50 +0100

    More namespace cleanup

--- CalculateurBackend/Equipement.cs
+++ CalculateurBackend/Equipement.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-public enum TypeArme { Improvisé = 0, Tranchant = 1, Contondant = 2, Flexible = 3, Perforant = 4 };
+﻿
 
 namespace Calculateur.Backend
 {
+
+    public enum TypeArme { Improvisé = 0, Tranchant = 1, Contondant = 2, Flexible = 3, Perforant = 4 };
     class Equipement
     {
 
