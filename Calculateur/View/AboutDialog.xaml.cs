3910f628d29cc4d1b24b11076992331db2f0a293
Julien Garagnon <jupotter.g@gmail.com>
2015-04-11 17:11:57 +0200

    Add about dialog

---
+++ Calculateur/View/AboutDialog.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculateur.View
+{
+    /// <summary>
+    /// Interaction logic for AboutDialog.xaml
+    /// </summary>
+    public partial class AboutDialog : Window
+    {
+        public AboutDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void Hyperlink_OnRequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            string uri = e.Uri.AbsoluteUri;
+            Process.Start(new ProcessStartInfo(uri));
+
+            e.Handled = true;
+        }
+    }
+}
