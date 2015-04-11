769e3fabdcbf29d303001ba3e0066c7d2ebf4d49
Inu <maximin.deschamps@laposte.net>
2015-04-11 15:17:47 +0200

    début magie magie

---
+++ CalculateurBackend/Liste de sort.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculateur.Backend
+{
+    class Liste_de_sort
+    {
+        List<Sort> grimo = new List<Sort>();
+        Character proprio;
+    }
+}
---
+++ CalculateurBackend/Sort.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculateur.Backend
+{
+    class Sort
+    {
+        List<Composante> effet;
+        List<int> ip;
+
+    }
+}
