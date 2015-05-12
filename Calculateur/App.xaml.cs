7d4672e8376d4a42a41f9127690e54f87999cf49
Julien Garagnon <jupotter.g@gmail.com>
2015-05-12 20:57:46 +0200

    Implement basic log4net function

--- Calculateur/App.xaml.cs
+++ Calculateur/App.xaml.cs
@@ -1,6 +1,10 @@
-﻿using Calculateur.View;
+﻿using System;
+using System.Windows.Threading;
+using Calculateur.View;
 using System.Windows;
 using Calculateur.Backend;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
 
 namespace Calculateur
 {
@@ -9,16 +13,30 @@ namespace Calculateur
     /// </summary>
     public partial class App : Application
     {
+        private static readonly log4net.ILog Log = log4net.LogManager.GetLogger(typeof(App));
+
         private void App_OnStartup(object sender, StartupEventArgs e)
         {
-            var window = new MainWindow();
             TalentsManager.Instance.Initialize();
             RacesManager.Instance.Initialize();
             ClassManager.Instance.Initialize();
             Bijouxmanager.Instance.Initialize();
+
+            this.DispatcherUnhandledException += OnUnhandledException;
+
+            Log.Info("Application started");
+
+            var window = new MainWindow();
             if (e.Args.Length != 0)
                 CharacterManager.Instance.Load(e.Args[0]);
             window.Show();
+        }
+
+        private void OnUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs dispatcherUnhandledExceptionEventArgs)
+        {
+            Log.Fatal(dispatcherUnhandledExceptionEventArgs.Exception);
+
+            Shutdown(1);
         }
     }
 }
